757b8392f13cb2a3b40b4c186d416bcee07fe628
christian-schw <Christianschwanse@gmx.de>
2024-10-05 10:38:39 -0400

    Implement Addons avTotalCost

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -31,6 +31,7 @@ const ConferenceEvent = () => {
             dispatch(decrementQuantity(index));
         }
     };
+
     const handleIncrementAvQuantity = (index) => {
         dispatch(incrementAvQuantity(index));
     };
@@ -46,22 +47,27 @@ const ConferenceEvent = () => {
     const getItemsFromTotalCost = () => {
         const items = [];
     };
-
     const items = getItemsFromTotalCost();
 
     const ItemsDisplay = ({ items }) => {
 
     };
+
     const calculateTotalCost = (section) => {
         let totalCost = 0;
         if (section === "venue") {
             venueItems.forEach((item) => {
                 totalCost += item.cost * item.quantity;
             });
+        } else if (section === "av") {
+            avItems.forEach((item) => {
+                totalCost += item.cost * item.quantity;
+            });
         }
         return totalCost;
     };
     const venueTotalCost = calculateTotalCost("venue");
+    const avTotalCost = calculateTotalCost("av");
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -170,7 +176,7 @@ const ConferenceEvent = () => {
                                         </div>
                                     ))}
                                 </div>
-                                <div className="total_cost">Total Cost:</div>
+                                <div className="total_cost">Total Cost: ${avTotalCost}</div>
                             </div>
 
                             {/* Meal Section */}
